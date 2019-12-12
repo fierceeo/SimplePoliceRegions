23e430b03ad9e2c9b5262fb4a1eec41941501036
BuildTools <unconfigured@null.spigotmc.org>
2019-12-11 22:47:17 -0800

    .

--- RamPolicePlugin/src/com/voidcitymc/www/main.java
+++ RamPolicePlugin/src/com/voidcitymc/www/main.java
@@ -29,5 +29,5 @@ public void onDisable() {
 }
 }
 
-
+// need to review this
 // https://www.spigotmc.org/threads/player-haspermission-not-working.146769/?__cf_chl_jschl_tk__=7095348855a540c73071771e519f7172fd209dcf-1575993858-0-AbnNQ7iSbPLxP1z0fpYy0K_zmvQtyErHCsq704IWp2NTEpvKmOktTWfQeQx-JGEDDTq7S9OXJaI0mUBY0g-NHsSnOiQ0Kn3ldwDmK21bnHhzczIkzOaTpkQT3WQJVwi2az4jQgpSA4t9mzbzo88WLwfLAzaj9EMOxhwG5UGIoTS2eRxfYFTykFsVqyWDrpnVto-70kIYk79uaREGwaICH0NtgytTPfMn6j_GlgPHjDhY5ll-Tm6-Z2BzlGJDQitNFMpqqTfDHQtfwVvo7PwcQeYonEIJOc71eH7Man8I53ZOljk_q2IbkpMuq8LaDNMeOFxpePpa57AS186S4Zf5pvE
