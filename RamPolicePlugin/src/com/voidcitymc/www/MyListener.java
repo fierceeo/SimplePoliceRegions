bc08032e30452939e290424b957bf297a2754332
ramdon_person <48343703+ramdon-person@users.noreply.github.com>
2019-12-13 08:31:58 -0800

    Update MyListener.java

--- RamPolicePlugin/src/com/voidcitymc/www/MyListener.java
+++ RamPolicePlugin/src/com/voidcitymc/www/MyListener.java
@@ -1,3 +1,4 @@
+/*
 package com.voidcitymc.www;
 
 import org.bukkit.event.EventHandler;
@@ -11,6 +12,38 @@ public class MyListener implements Listener {
 	
     @EventHandler
 	public void onDamage(EntityDamageByEntityEvent event) {
+    	worker work = new worker();
+    	if (event.getEntity() instanceof Player && event.getEntity() instanceof Player) {
+    		//check if the player who punched someone is a police and has the police bitan
+    		Player damager = (Player) event.getDamager();
+    		if (work.allreadyPolice(work.playerToUUID(work.playerToString(damager)) ) //put stuff here too) {
+    			
+    		}
+    	
+    		
+    		
+    		
+    		
+    	}
+	}
+	
+	
+}
+*/
+
+package com.voidcitymc.www;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class MyListener implements Listener {
+	
+    @EventHandler
+	public void onDamage(Entity damager, Entity damagee, EntityDamageEvent.DamageCause cause, double damage) {
     	worker work = new worker();
     	if (event.getEntity() instanceof Player && event.getEntity() instanceof Player) {
     		//check if the player who punched someone is a police and has the police bitan
