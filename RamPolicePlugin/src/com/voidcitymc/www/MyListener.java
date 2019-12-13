ce38133e52ffb1cde7d4feccac152ad17ea93034
ramdon_person <48343703+ramdon-person@users.noreply.github.com>
2019-12-12 22:10:54 -0800

    Update MyListener.java

--- RamPolicePlugin/src/com/voidcitymc/www/MyListener.java
+++ RamPolicePlugin/src/com/voidcitymc/www/MyListener.java
@@ -12,7 +12,7 @@ public class MyListener implements Listener {
     @EventHandler
 	public void onDamage(EntityDamageByEntityEvent event) {
     	worker work = new worker();
-    	if (event.getDamager() instanceof player && event.getDamagee() instanceof player) {
+    	if (event.getDamager() instanceof Player && event.getDamagee() instanceof player) {
     		//check if the player who punched someone is a police and has the police bitan
     		if (work.allreadyPolice(work.playerToUUID(work.playerToString(event.getDamager()))) //put stuff here too) {
     			
