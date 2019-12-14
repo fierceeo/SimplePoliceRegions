9a424353a25c015582cbc86a99b1a844dd1efc72
ramdon-person <fierceeo@gmail.com>
2019-12-14 00:19:29 -0800

    Work in progress of adding messages to the listner and checking if the police has a stick in their main hand

--- RamPolicePlugin/src/com/voidcitymc/www/MyListener.java
+++ RamPolicePlugin/src/com/voidcitymc/www/MyListener.java
@@ -3,6 +3,7 @@ package com.voidcitymc.www;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 //import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -12,12 +13,19 @@ public class MyListener implements Listener {
     @EventHandler
 	public void onDamage(EntityDamageByEntityEvent event) {
     	worker work = new worker();
-    	if (event.getEntity() instanceof Player && event.getEntity() instanceof Player) {
+    	if (event.getEntity() instanceof Player /* for testing purpases I commented this out, so I can test on npcs && event.getDamager() instanceof Player */ ) {
     		//check if the player who punched someone is a police and has the police bitan
     		Entity damagerE = (Entity) event.getDamager();
     		Player damagerP = (Player) damagerE;
-    		if (work.alreadyPolice(damagerP.getUniqueId().toString()) ) /*put stuff here too) */ {
+    		Entity damageeE = (Entity) event.getEntity();
+    		Player damageeP = (Player) damageeE;
+    		ItemStack Stick = new ItemStack();
+    		//damager is the police
+    		//damagee is the criminal
+    		if (work.alreadyPolice(damagerP.getUniqueId().toString()) && damagerP.getItemInMainHand​()  ) /*put stuff here too) */ {
     			System.out.println("A player has been arrested");
+    			damagerP.sendMessage("You have arrested "+ damageeP.getName());
+    			damageeP.sendMessage("You have been arrested");
     		}
     	
     		
